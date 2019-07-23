7e10d148e1bfa32d3a0a25882ae9b93c26cc3f9f
涂 家 浩 <henry88819@gmail.com>
2019-07-24 00:45:42 +0800

    Fixed Arrow Key Icons

--- app/src/main/java/c/jahhow/remotecontroller/HelpFragment.java
+++ app/src/main/java/c/jahhow/remotecontroller/HelpFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,15 @@ public class HelpFragment extends Fragment {
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.help, container, false);
+		View layout = inflater.inflate(R.layout.help, container, false);
+		layout.findViewById(R.id.buttonOpenConnectionGuide).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getActivity().getSupportFragmentManager().beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+						.addToBackStack(null)
+						.replace(android.R.id.content, new ConnectionGuideFragment()).commit();
+			}
+		});
+		return layout;
 	}
 }
