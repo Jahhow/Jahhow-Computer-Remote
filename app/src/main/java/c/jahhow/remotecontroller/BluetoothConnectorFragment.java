01f3e190a51423319e08f1291b2f358b034368e7
涂 家 浩 <35959817+Jahhow@users.noreply.github.com>
2020-01-22 22:41:49 +0800

    Bluetooth Connector

--- app/src/main/java/c/jahhow/remotecontroller/BluetoothConnectorFragment.java
+++ app/src/main/java/c/jahhow/remotecontroller/BluetoothConnectorFragment.java
@@ -1,23 +1,21 @@
 package c.jahhow.remotecontroller;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class BluetoothConnectorFragment extends Fragment {
-
-
-    public BluetoothConnectorFragment() {
-        // Required empty public constructor
-    }
-
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(getClass().getSimpleName(), bluetoothAdapter == null ? "bluetoothAdapter==null" : "bluetoothAdapter!=null");
         return inflater.inflate(R.layout.fragment_bluetooth_connector, container, false);
     }
 }
